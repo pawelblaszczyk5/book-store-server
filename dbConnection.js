a1bfbe6fe7922d5cea47d2a3db4ff849283fef34
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2020-12-20 17:22:43 +0100

    Refactored server and addded reviews handling

---
+++ dbConnection.js
@@ -0,0 +1,57 @@
+const mongo = require("mongodb").MongoClient;
+let database;
+let booksCollection, reviewsCollection;
+
+const init = () => {
+    mongo.connect(
+        `mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.USER}`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        },
+        (err, client) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            database = client.db(`${process.env.USER}`);
+            booksCollection = database.collection('products');
+            reviewsCollection = database.collection('reviews');
+        }
+    );
+};
+
+const getBooksLimitAndSkip = (limit, skip) => {
+    return new Promise((resolve) => {
+        booksCollection
+            .find()
+            .limit(limit)
+            .skip(skip)
+            .toArray((err, items) => {
+                resolve(items);
+            });
+    });
+};
+
+const getBookById = (idToSearch) => {
+    return new Promise((resolve) => {
+        booksCollection
+            .findOne({id: idToSearch}, (err, item) => {
+                resolve(item);
+            });
+    })
+};
+
+const getReviewsByBookId = (idToSearch) => {
+    return new Promise((resolve) => {
+        reviewsCollection
+            .find({bookId: idToSearch}).toArray((err, item) => {
+            resolve(item);
+        });
+    })
+}
+
+module.exports.init = init;
+module.exports.getBooksLimitAndSkip = getBooksLimitAndSkip;
+module.exports.getBookById = getBookById;
+module.exports.getReviewsByBookId = getReviewsByBookId;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const mongo = require("mongodb").MongoClient;
+const database = require('./dbConnection');
+const routes = require('./routes')
+
 require("dotenv").config();
-const url = `mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.USER}`;
-let databse;
-let collection;
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
 });
@@ -17,43 +17,7 @@ app.use((req, res, next) => {
     );
     next();
 });
-app.get("/", (req, res) => {
-    res.send("Hello to my API");
-});
-app.get("/getall", (req, res) => {
-    collection.find().toArray((err, items) => {
-        res.json(items);
-    });
-});
-app.get("/limitSkip/:limit/:skip", function (req, res) {
-    collection
-        .find()
-        .limit(+req.params.limit)
-        .skip(+req.params.skip)
-        .toArray((err, items) => {
-            res.json(items);
-        });
-});
-app.get("/getById/:id/", function (req, res) {
-    collection.findOne({ id: +req.params.id }, (err, item) => {
-        res.json(item);
-    });
-});
-const init = () => {
-    mongo.connect(
-        url,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        },
-        (err, client) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            databse = client.db(`${process.env.USER}`);
-            collection = databse.collection("produkty");
-        }
-    );
-};
-init();
+
+app.use('/', routes);
+
+database.init();
